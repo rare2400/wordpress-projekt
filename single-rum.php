94ba9de29b5c0a7feb900d63129c465e95480201
Ramona Reinholdz <rare2400@student.miun.se>
2026-06-04 22:08:14 +0200

    förbättrar struktur för rum genom att justera HTML-element och rubriker

--- single-rum.php
+++ single-rum.php
@@ -20,7 +20,7 @@
                     <!-- Information about the room, fetched from custom fields -->
                     <div class="single-meta card card-body">
 
-                        <h2><?php esc_html_e("Information", "skogsglantan"); ?></h2>
+                        <h1>Information</h1>
 
                         <ul>
                             <li><strong>Pris per natt:</strong> <?php echo esc_html(get_field("pris_per_natt")) ?> kr</li>
@@ -32,7 +32,7 @@
 
                     <!-- Description -->
                     <article class="room-description card">
-                        <h1><?php the_title(); ?></h1>
+                        <h2><?php the_title(); ?></h2>
                         <?php the_content(); ?>
 
                     </article>
@@ -40,9 +40,7 @@
 
                     <!-- CTA -->
                     <div class="booking-cta">
-                        <a href="<?php echo esc_url(home_url("/kontakt")); ?>" class="secondary-btn">
-                            <?php esc_html_e("Boka nu", "skogsglantan"); ?>
-                        </a>
+                        <a href="<?php echo esc_url(home_url("/kontakt")); ?>" class="secondary-btn">Boka nu</a>
                     </div>
 
                 </div>
@@ -55,7 +53,7 @@
                         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 640 640" width="20" height="20" class="icon" aria-hidden="true"><!--!Font Awesome Free v7.2.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2026 Fonticons, Inc.-->
                             <path d="M73.4 297.4C60.9 309.9 60.9 330.2 73.4 342.7L233.4 502.7C245.9 515.2 266.2 515.2 278.7 502.7C291.2 490.2 291.2 469.9 278.7 457.4L173.3 352L544 352C561.7 352 576 337.7 576 320C576 302.3 561.7 288 544 288L173.3 288L278.7 182.6C291.2 170.1 291.2 149.8 278.7 137.3C266.2 124.8 245.9 124.8 233.4 137.3L73.4 297.3z" />
                         </svg>
-                        <?php esc_html_e("Tillbaka till alla rum", "skogsglantan"); ?>
+                        Tillbaka till alla rum
                     </a>
                 </div>
 
